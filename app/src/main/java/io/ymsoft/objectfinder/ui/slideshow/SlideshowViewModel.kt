98726267e5c25c121ee816b84de56b1c3d374360
dev-young <kym1340@gmail.com>
2020-05-11 18:33:34 +0900

    프로젝트 생성

---
+++ app/src/main/java/io/ymsoft/objectfinder/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package io.ymsoft.objectfinder.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
